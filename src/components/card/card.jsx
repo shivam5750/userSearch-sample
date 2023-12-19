cc9b2636d47d07be1de826c3bb9d70419152fcfe
unknown <vyasb2905@gmail.com>
2023-12-19 17:20:34 +0530

    updated the character

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -11,7 +11,7 @@ class Card extends Component {
                 <div key={user.id} className="card-container">
                     <img 
                     alt= {`${name}`}
-                    src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                    src={`https://robohash.org/${id}?set=set4&size=180x180`}
                     />
                     <p>{`${name}`}</p>
                     <p>{`${email}`}</p>
